1641fe7a81666c657627deb778907737148599d4
samcas1 <paulascasillas@hotmail.com>
2020-05-15 12:35:28 -0500

    Modify Beta.0.1 lineation

--- Beta.0.1.py
+++ Beta.0.1.py
@@ -55,6 +55,7 @@ def display_battery(img_equ):
 
     return img_equ # Return the frame with the battery
 
+
 def procesing(frame):
     """Track the color in the frame"""
     # construct the argument parse and parse the arguments
