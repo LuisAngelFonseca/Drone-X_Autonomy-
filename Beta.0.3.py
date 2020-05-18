81b55db54c2bf3b0dc3f548d02ef3343f1db4ffe
Kamil <a01702974@itesm.mx>
2020-05-18 10:40:46 -0500

    Prueba de implementacion de vision con circularidad

--- Beta.0.3.py
+++ Beta.0.3.py
@@ -16,22 +16,25 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
                     help='** = required')
 parser.add_argument('-D', "--debug", action='store_true',
-    help='add the -D flag to enable debug mode. Everything works the same, but no commands will be sent to the drone')
+                    help='add the -D flag to enable debug mode. Everything works the same, but no commands will be sent to the drone')
 
 args = parser.parse_args()
 
 # Speed of the drone
 S = 40
-#Factor de velocidad
+# Factor de velocidad
 oSpeed = 1
+center = None
+x, y, radius = 0, 0, 0
 
 
 def callback(x):
     pass
 
+
 def display_grid(frame):
     """Display grid on the video"""
-    #Display each line of the dynamic grid
+    # Display each line of the dynamic grid
     # Display each line of the greed
     x1 = int(480 - (r + 40))
     x2 = int(480 + (r + 40))
@@ -41,7 +44,8 @@ def display_grid(frame):
     cv2.line(frame, pt1=(x2, 0), pt2=(x2, 720), color=(255, 0, 0), thickness=2)
     cv2.line(frame, pt1=(0, y1), pt2=(960, y1), color=(255, 0, 0), thickness=2)
     cv2.line(frame, pt1=(0, y2), pt2=(960, y2), color=(255, 0, 0), thickness=2)
-    return x1, x2, y1, y2, frame # Return the position of each line and the frame
+    return x1, x2, y1, y2, frame  # Return the position of each line and the frame
+
 
 def display_text(img_equ):
     """Display text in the video"""
@@ -51,20 +55,21 @@ def display_text(img_equ):
     cv2.putText(img_equ, text='Drone-X', org=(410, 25), fontFace=font, fontScale=1, color=(0, 0, 0),
                 thickness=2, lineType=cv2.LINE_8)
 
-    return img_equ #Return the frame with the text
+    return img_equ  # Return the frame with the text
+
 
 def display_battery(img_equ):
     """Display a battery in the video that indicate the percentage of battery"""
     # Display a battery in the image
-    cv2.rectangle(img_equ, pt1=(920, 5), pt2=(950, 25), color=(255, 255, 255), thickness= 2)
+    cv2.rectangle(img_equ, pt1=(920, 5), pt2=(950, 25), color=(255, 255, 255), thickness=2)
     cv2.rectangle(img_equ, pt1=(950, 9), pt2=(955, 21), color=(255, 255, 255), thickness=2)
 
-    global tiempo_elapsed, tiempo_actual,battery
+    global tiempo_elapsed, tiempo_actual, battery
 
-    #la primera vez la bateria es 100, pero este valor solo dura 5 segundos
+    # la primera vez la bateria es 100, pero este valor solo dura 5 segundos
     battery = 100
     if not args.debug:
-        if tiempo_actual - tiempo_elapsed > 6 :
+        if tiempo_actual - tiempo_elapsed > 6:
             tiempo_elapsed = tiempo_actual
             print("Solicitar Bateria ")
             try:
@@ -76,7 +81,7 @@ def display_battery(img_equ):
             tiempo_actual = int(time.time())
             battery = battery
     elif args.debug:
-        if tiempo_actual - tiempo_elapsed > 24 :
+        if tiempo_actual - tiempo_elapsed > 24:
             tiempo_elapsed = tiempo_actual
             print("Solicitar Bateria Debug")
             try:
@@ -88,93 +93,79 @@ def display_battery(img_equ):
             tiempo_actual = int(time.time())
             battery = battery
 
-
     # Display a complete battery
     if battery > 75:
         cv2.rectangle(img_equ, pt1=(924, 9), pt2=(930, 21), color=(0, 255, 0), thickness=-1)
         cv2.rectangle(img_equ, pt1=(932, 9), pt2=(938, 21), color=(0, 255, 0), thickness=-1)
         cv2.rectangle(img_equ, pt1=(940, 9), pt2=(947, 21), color=(0, 255, 0), thickness=-1)
-    #Display a 2/3 of the battery
+    # Display a 2/3 of the battery
     elif battery < 75 and battery > 50:
         cv2.rectangle(img_equ, pt1=(924, 9), pt2=(930, 21), color=(0, 255, 255), thickness=-1)
         cv2.rectangle(img_equ, pt1=(932, 9), pt2=(940, 21), color=(0, 255, 255), thickness=-1)
-    #Display 1/3 of the battery
+    # Display 1/3 of the battery
     elif battery < 50 and battery > 25:
         cv2.rectangle(img_equ, pt1=(924, 9), pt2=(930, 21), color=(0, 0, 255), thickness=-1)
 
-
     return img_equ
 
-def procesing(frame):
+
+def processing(frame):
     """Track the color in the frame"""
-    # construct the argument parse and parse the arguments
 
-    blurred = cv2.GaussianBlur(frame, (11,11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    # Color range of wanted object
+    color_lower = (90, 80, 55)
+    color_upper = (107, 251, 255)
 
-    # Lemmons color range
-    greenLower = (90, 80, 55)
-    greenUpper = (107, 251, 255)
+    # blur it, and convert it to the HSV color space
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    frameHSV = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-    # (29, 86, 6)
-    # (64, 255, 255)
-    # (90, 80, 55)
-    # (107, 251, 255)
-    buffer = 64
-    pts = deque(maxlen=buffer)
-
-    # construct a mask for the color "green", then perform
+    # construct a mask for the color, then perform
     # a series of dilations and erosions to remove any small
     # blobs left in the mask
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.inRange(frameHSV, color_lower, color_upper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
 
     # find contours in the mask and initialize the current
     # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    center = None
+    contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    global center, x, y, radius
+
+    contours_circles = []
 
     # only proceed if at least one contour was found
-    if len(cnts) > 0:
+    if len(contours) > 0:
+        for contour in contours:
+            perimeter = cv2.arcLength(contour, True)
+            area = cv2.contourArea(contour)
+            circularity = 4 * math.pi * (area / (perimeter * perimeter))
+            if 0.85 < circularity < 1.05:
+                contours_circles.append(contour)
+
+    if len(contours_circles) > 0:
         # find the largest contour in the mask, then use
         # it to compute the minimum enclosing circle and
         # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
+        centroid = max(contours_circles, key=cv2.contourArea)
+        ((x_circle, y_circle), radius) = cv2.minEnclosingCircle(centroid)
+        M = cv2.moments(centroid)
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        x, y = center
 
         # only proceed if the radius meets a minimum size
-        if radius > 10:
-            # draw the circle and centroid on the frame,
-            # then update the list of tracked points
-            cv2.circle(frame, (int(x), int(y)), int(radius),
-                       (0, 255, 255), 2)
+        if radius > 5:
+            # draw the circle and centroid on the frame
+            cv2.circle(frame, (int(x_circle), int(y_circle)), int(radius), (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
     else:
         x = 480
         y = 360
-        radius = 30
+        radius = 300
 
-    # update the points queue
-    pts.appendleft(center)
+    return (x, y, radius, frame)  # Return the position and radius of the object and also the frame
 
-    # loop over the set of tracked points
-    for i in range(1, len(pts)):
-        # if either of the tracked points are None, ignore
-        # them
-        if pts[i - 1] is None or pts[i] is None:
-            continue
-
-        # otherwise, compute the thickness of the line and
-        # draw the connecting lines
-        thickness = int(np.sqrt(buffer / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-
-    return(x, y, radius, frame) # Return the position and radius of the object and also the frame
 
 def track_drone_track(x, y, limitx1, limitx2, limity1, limity2):
     """Return instruction on how to move to the drone based on position and the greed """
@@ -208,6 +199,7 @@ def track_drone_track(x, y, limitx1, limitx2, limity1, limity2):
 
     return dirr  # Return the instruction number from 0-8
 
+
 def susana_distancia(r):
     if r > 20 and r < 40:
         mov = 0
@@ -217,7 +209,8 @@ def susana_distancia(r):
         mov = 2
     else:
         mov = 0
-    return mov # Return an instruction from 0 - 2
+    return mov  # Return an instruction from 0 - 2
+
 
 def drone_stay_close(dir, mov, velocity1, velocity2):
     """Control velocities to track object"""
@@ -270,13 +263,14 @@ def drone_stay_close(dir, mov, velocity1, velocity2):
     # Send the velocities to drone
     return left_right_velocity, for_back_velocity, up_down_velocity, yaw_velocity
 
+
 def dinamic_speed(x, y):
     """
     Calculate the distance between the center and the position of the object
     then is multiplied by a factor that return a velocitie near 0 if the object is near the center of the screen
     and if the object is near the edges return a value near 70
     """
-    velocidad = int((math.sqrt((x-480)**2+(y-300)**2))*(70/600))
+    velocidad = int((math.sqrt((x - 480) ** 2 + (y - 300) ** 2)) * (70 / 600))
 
     return velocidad
 
@@ -285,7 +279,7 @@ def dinamic_speed(x, y):
 # Create an instance of Drone Tello
 tello = Tello()
 
-#Connect to Drone
+# Connect to Drone
 tello.connect()
 
 # Send message to drone to start stream
@@ -306,7 +300,6 @@ counter = 0
 # This variable will send a number from 0 to 8 each number tell the drone how to move
 dir = 0
 
-
 #########################
 #       #       #       #
 #   5   #   3   #   6   #
@@ -324,7 +317,7 @@ dir = 0
 # Frames per second
 FPS = 25
 
-#Create 2 variables that count time
+# Create 2 variables that count time
 tiempo_actual = int(time.time())
 tiempo_elapsed = int(time.time())
 
@@ -345,10 +338,10 @@ while True:
     # Capture a frame from drone camera
     frame_read = tello.get_frame_read()
 
-    #esta variable se encarga de decidir cuando corre el main verdadero y cuando no
+    # esta variable se encarga de decidir cuando corre el main verdadero y cuando no
     Main_Real = False
     frameCount = 0
-    #esta variable hace que puedas controlar al dron con la barra espaciadora
+    # esta variable hace que puedas controlar al dron con la barra espaciadora
     OVERRIDE = False
     tello.get_battery()
 
@@ -376,7 +369,7 @@ while True:
         cv2.createTrackbar('lowV', 'Config', ilowV, 255, callback)
         cv2.createTrackbar('highV', 'Config', ihighV, 255, callback)
 
-    #aqui van las cosas que irian en el main normal
+    # aqui van las cosas que irian en el main normal
     while not Main_Real:
 
         # Function that updates dron speeds
@@ -413,8 +406,8 @@ while True:
             # mask = np.rot90(mask)
 
             cv2.imshow('mask', mask)
-            #print(ilowH, ilowS, ilowV)
-            #print(ihighH, ihighS, ihighV)
+            # print(ilowH, ilowS, ilowV)
+            # print(ihighH, ihighS, ihighV)
 
         # Delay to showcase desired fps in video
         time.sleep(1 / FPS)
@@ -492,7 +485,7 @@ while True:
             break
 
         # Getting the position of the object, radius and tracking the object in the frame
-        x, y, r, video = procesing(frame)
+        x, y, r, video = processing(frame)
 
         # Display grid in the actual frame, take video and radius of the object as arguments
         # return the grid dynamic position first line passing through  x_1 ..... last line trough y_2
