c7b6209ba6549ba297bc05a0176c1540c505c20f
Luis Angel Fonseca <62559929+LuisAngelFonseca@users.noreply.github.com>
2020-05-21 17:46:45 -0500

    El bueno

--- Beta.0.3.py
+++ Beta.0.3.py
@@ -1,5 +1,4 @@
 from djitellopy import Tello
-from collections import deque
 import cv2
 import numpy as np
 import time
@@ -10,11 +9,9 @@ import os
 import datetime
 from numpy.core.multiarray import ndarray
 
-"""
-Esta parte esta encargada de los argumetnos que se brindan cuando corres este script desde la linea de comando
-al agregarle el -D se corre en version de debugeo donde se pueden cambiar los valores de hsv de acorde a la camara
-del dron
-"""
+
+# We declare the arguments when we run the script with the command line "-d" which execute the debug mode,
+# he we can change the HSV values to calibrate the target HSV values
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, add_help=False)
 parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
                     help='** = required')
@@ -22,20 +19,14 @@ parser.add_argument('-D', "--debug", action='store_true',
                     help='add the -D flag to enable debug HSV Mode, drone will act as a camera to improve HSV Calibration')
 parser.add_argument('-ss', "--save_session", action='store_true',
                     help='add the -ss flag to save your sessions in order to have your tello sesions recorded')
-
 args = parser.parse_args()
 
-# Speed of the drone
-S = 40
-# Factor de velocidad
-oSpeed = 1
-
-
 def callback(x):
+    """ Function declare for the trackbars to work """
     pass
 
-
 def stackImages(scale, imgArray):
+    """ This function is for the debug mode that allows to have 4 images stack in the same window """
     rows = len(imgArray)
     cols = len(imgArray[0])
     rowsAvailable = isinstance(imgArray[0], list)
@@ -67,10 +58,9 @@ def stackImages(scale, imgArray):
         ver = hor
     return ver
 
-
 def display_grid(frame, size, x, y):
-    """Display grid on the video"""
-    # Display each line of the dynamic grid
+    """ Display grid on the frame and display two lines from the center to the tracking object """
+    # You can give a value to size to change the grid sizes
     x1 = int(480 - (size))
     x2 = int(480 + (size))
     y1 = int(360 - (size * (3 / 4)) - 60)
@@ -82,14 +72,14 @@ def display_grid(frame, size, x, y):
     if x == None or y == None:
         x = 480
         y = 360
+    # This part draw two lines from the center to the target
     cv2.line(frame, pt1=(int(x), int(y)), pt2=(480, int(y)), color=(0, 255, 0), thickness=2)
     cv2.line(frame, pt1=(int(x), int(y)), pt2=(int(x), 360), color=(0, 255, 0), thickness=2)
 
     return x1, x2, y1, y2, frame  # Return the position of each line and the frame
 
-
 def display_text(frame_equ):
-    """Display text in the video"""
+    """ Display text in the video """
     # Diplay text in the image
     font = cv2.FONT_ITALIC
 
@@ -98,19 +88,17 @@ def display_text(frame_equ):
 
     return frame_equ  # Return the frame with the text
 
-
 def display_battery(frame_equ):
-    """Display a battery in the video that indicate the percentage of battery"""
+    """ Display a battery in the video that indicate the percentage of battery """
     # Display a battery in the image
     cv2.rectangle(frame_equ, pt1=(920, 5), pt2=(950, 25), color=(255, 255, 255), thickness=2)
     cv2.rectangle(frame_equ, pt1=(950, 9), pt2=(955, 21), color=(255, 255, 255), thickness=2)
 
     global tiempo_elapsed, tiempo_actual, battery
 
-    # la primera vez la bateria es 100, pero este valor solo dura 5 segundos
-    battery = 100
+    # Request battery every 15 seconds in autonomous mode
     if not args.debug:
-        if tiempo_actual - tiempo_elapsed > 6:
+        if tiempo_actual - tiempo_elapsed > 15:
             tiempo_elapsed = tiempo_actual
             print("Solicitar Bateria ")
             try:
@@ -118,9 +106,10 @@ def display_battery(frame_equ):
             except:
                 battery = 0
         else:
-            tiempo_elapsed = tiempo_elapsed
+            # tiempo_elapsed = tiempo_elapsed
             tiempo_actual = int(time.time())
-            battery = battery
+            #battery = battery
+    # Request battery every 24 seconds in debug mode
     elif args.debug:
         if tiempo_actual - tiempo_elapsed > 24:
             tiempo_elapsed = tiempo_actual
@@ -130,9 +119,9 @@ def display_battery(frame_equ):
             except:
                 battery = 0
         else:
-            tiempo_elapsed = tiempo_elapsed
+            # tiempo_elapsed = tiempo_elapsed
             tiempo_actual = int(time.time())
-            battery = battery
+            #battery = battery
 
     # Display a complete battery
     if battery > 75:
@@ -149,9 +138,8 @@ def display_battery(frame_equ):
 
     return frame_equ
 
-
-def processing(frame, lower_hsv, upper_hsv):
-    """Track the color in the frame"""
+def object_detection(frame, lower_hsv, upper_hsv):
+    """ Track the color in the frame """
 
     if args.debug:
         color_lower = lower_hsv
@@ -213,15 +201,22 @@ def processing(frame, lower_hsv, upper_hsv):
 
     return x, y, radius, detection, frame  # Return the position and radius of the object and also the frame
 
-
 def drone_stay_close(x, y, limitx1, limitx2, limity1, limity2, r, distanceradius, tolerance):
-    """Control velocities to track object"""
+    """
+    Control velocities to track object, take x and y for the position of the target,
+    limitx1 to limity2 are the lines of the center square of the grid
+    r is the radius of the object
+    distanceradius is the radius were you want the drone to stop
+    tolerance is the range where the drone can keep the distance from the target
+    """
     global left_right_velocity, for_back_velocity, up_down_velocity, yaw_velocity
-
+    # If the target is in the center, the drone can move back or forward
     if x < limitx2 and x > limitx1 and y < limity2 and y > limity1:
         for_back_velocity = int((distanceradius - r) * 1.3333)
+        # If the drone is centered with the target and at the distance send velocities to 0
         if r < distanceradius + tolerance and r > distanceradius - tolerance:
             for_back_velocity = 0
+    # Drone move to get the target centered
     else:
         yaw_velocity = int((x - 480) * .125)
         up_down_velocity = int((360 - y) * .1388888)
@@ -231,39 +226,45 @@ def drone_stay_close(x, y, limitx1, limitx2, limity1, limity2, r, distanceradius
     return yaw_velocity, up_down_velocity, for_back_velocity
 
 # Setup
-# Create an instance of Drone Tello
+# Create an instance of a Drone from the Tello library
 tello = Tello()
-
+# Variable to start the velocities control if True
 send_rc_control = False
-
-# Create a counter for the takeoff and activate rc control
-counter = 0
-
-# Frames per second
+# Create a takeoff_timer for the takeoff and activate rc control
+takeoff_timer = 0
+# Frames per second of the stream
 FPS = 25
+# Frames per second for the video capture
 FPS_vid = 10
+# Set the speed for the override mode
+S = 40
+# Speed factor that increase the value for override
+oSpeed = 1
+# Battery value
+battery = 0
+
+# Values are given in pixels for following variables
+# Grid size
+grid_size = 100
+# Radius of the object in which the drone will stop
+radius_stop = 40
+# Tolerance range in which the drone will stop
+radius_stop_tolerance = 5
 
 # Create 2 variables that count time
 tiempo_actual = int(time.time())
 tiempo_elapsed = int(time.time())
 
-#
-init_time = int(time.time())
-turn_time = 0
-
+# Main Loop
 while True:
     # Restore values to 0, to clean past values
     left_right_velocity = 0
     for_back_velocity = 0
     up_down_velocity = 0
     yaw_velocity = 0
-    speed = 10
 
     if not tello.connect():
         print("Tello not connected")
-
-    if not tello.set_speed(speed):
-        print("Not set speed to lowest possible")
 
     if not tello.streamoff():
         print("Could not stop video stream")
@@ -273,20 +274,19 @@ while True:
 
     # Capture a frame from drone camera
     frame_read = tello.get_frame_read()
-
+    # Take the original frame for the video capture of the session
     frame_original = frame_read.frame
 
-    # esta variable se encarga de decidir cuando corre el main verdadero y cuando no
-    # Lo que esta afuera del while true solo correra una vez
-    Ciclo_Dron = False
-    frame_count = 0
-    # esta variable hace que puedas controlar al dron con la barra espaciadora
+    # Infinite cycle to run code
+    dron_continuos_cycle = True
+    # You change the value with the spacebar and give you the manual control of the drone
     OVERRIDE = False
-    # check tello battery
-    tello.get_battery()
+    # Check tello battery before starting
+    battery = int(tello.get_battery())
 
     # This checks if we are in the debug mode,
     if args.debug:
+        # Start debug mode, to calibrate de HSV values
         print("DEBUG MODE ENABLED!")
 
         # create trackbars for color change
@@ -304,44 +304,44 @@ while True:
         # ITERATIONS
         cv2.createTrackbar('Erosion', 'Color Calibration', 0, 30, callback)
         cv2.createTrackbar('Dilation', 'Color Calibration', 0, 30, callback)
-
+    # Check if save session mode is on 
     if args.save_session:
 
         # If we are to save our sessions, we need to make sure the proper directories exist
         ddir = "Sessions"
-
+        # If the provided path is not found, we create a new one
         if not os.path.isdir(ddir):
             os.mkdir(ddir)
+        # We create a new folder path to save the actual video session with day and time
         ddir = "Sessions/Session {}".format(str(datetime.datetime.now()).replace(':', '-').replace('.', '_'))
         os.mkdir(ddir)
-
+        # Get the frame size 
         width = frame_original.shape[1]
         height = frame_original.shape[0]
-
+        # We create two writer objects that create the video sessions 
         writer = cv2.VideoWriter("{}/TelloVideo.avi".format(ddir), cv2.VideoWriter_fourcc(*'XVID'),
-                                 FPS_vid, (width, height))  # con esta wea se guardan videos
-        writer_proccesed = cv2.VideoWriter("{}/TelloVideo_processed.avi".format(ddir), cv2.VideoWriter_fourcc(*'XVID'),
-                                           FPS_vid, (width, height))  # con esta wea se guardan videos
+                                 FPS_vid, (width, height))  
+        writer_processed = cv2.VideoWriter("{}/TelloVideo_processed.avi".format(ddir), cv2.VideoWriter_fourcc(*'XVID'),
+                                           FPS_vid, (width, height))  
 
-    # aqui van las cosas que irian en el main normal
-    while not Ciclo_Dron:
+    # This cycle is intended to run continuosly
+    while dron_continuos_cycle:
 
-        # Function that updates dron speeds
+        # Function that updates dron velocities in the override mode and autonomous mode 
         if send_rc_control:
             tello.send_rc_control(left_right_velocity, for_back_velocity, up_down_velocity, yaw_velocity)
-
+        # If theres no frame, do not read the actual frame 
         if frame_read.stopped:
             frame_read.stop()
             break
 
-        # Frame read
+        # Update frame 
         frame_original = frame_read.frame
         frame = frame_original.copy()
-
-        frame_count += 1
-
+        
+        # Update the track bars HSV mask and apply the erosion and dilation
         if args.debug:
-            # Read all the trackbars positions
+            # Read all the track bars positions
             h_min = cv2.getTrackbarPos('Hue Min', 'Color Calibration')
             h_max = cv2.getTrackbarPos('Hue Max', 'Color Calibration')
             s_min = cv2.getTrackbarPos('Sat Min', 'Color Calibration')
@@ -350,6 +350,7 @@ while True:
             v_max = cv2.getTrackbarPos('Val Max', 'Color Calibration')
             erosion = cv2.getTrackbarPos('Erosion', 'Color Calibration')
             dilation = cv2.getTrackbarPos('Dilation', 'Color Calibration')
+            
             # Apply a Gaussian Blur to the image in order to reduce detail
             blurred = cv2.GaussianBlur(frame, (11, 11), 0)
             # Create HSV image, passing it from BGR
@@ -367,36 +368,35 @@ while True:
             frameStack = stackImages(0.4, ([frame, frame_HSV], [mask, frameResult]))
 
             cv2.imshow('Stacked Images', frameStack)
-
-        # Delay to showcase desired fps in video
-        time.sleep(1 / FPS)
-
+        
+        # Wait for a key to be press and grabs the value
         k = cv2.waitKey(20)
 
-        # Drone Takeoff
-        if counter == 40:
+        # Drone Takeoff if the timer get to 40
+        if takeoff_timer == 40:
             if not args.debug:
-                print("Preparado pa la guerra")
-                tello.takeoff()
+                print("Takeoff...")
                 tello.get_battery()
+                tello.takeoff()
             send_rc_control = True
 
-        # Press T to take off
-        if k == ord('t') and counter > 50:
+        # Press T to take off in override mode 
+        if k == ord('t') and takeoff_timer > 50:
             if not args.debug:
-                print("Preparado la guerra manual")
-                tello.takeoff()
+                print("Override mode: Takeoff...")
                 tello.get_battery()
+                tello.takeoff()
+
             send_rc_control = True
 
         # Press L to land
         if k == ord('l'):
             if not args.debug:
-                print("A mimir")
+                print("Override mode: Land...")
                 tello.land()
             send_rc_control = False
 
-        # Press Backspace for controls override
+        # Press spacebar to enter override mode
         if k == 32:
             if not OVERRIDE:
                 OVERRIDE = True
@@ -406,7 +406,7 @@ while True:
                 print("OVERRIDE DISABLED")
 
         if OVERRIDE:
-            # S & W to fly forward & back
+            # W to fly forward and S to fly back 
             if k == ord('w'):
                 for_back_velocity = int(S * oSpeed)
             elif k == ord('s'):
@@ -414,7 +414,7 @@ while True:
             else:
                 for_back_velocity = 0
 
-            # a & d to pan left & right
+            # Z to fly clockwise and C to fly counter clockwise
             if k == ord('z'):
                 yaw_velocity = int(S * oSpeed)
             elif k == ord('c'):
@@ -422,15 +422,15 @@ while True:
             else:
                 yaw_velocity = 0
 
-            # Q & E to fly up & down
-            if k == ord('e'):
+            # Q to fly up and E to fly down
+            if k == ord('q'):
                 up_down_velocity = int(S * oSpeed)
-            elif k == ord('q'):
+            elif k == ord('e'):
                 up_down_velocity = -int(S * oSpeed)
             else:
                 up_down_velocity = 0
 
-            # c & z to fly left & right
+            # A to fly left and D to fly right
             if k == ord('a'):
                 left_right_velocity = int(S * oSpeed)
             elif k == ord('d'):
@@ -438,32 +438,37 @@ while True:
             else:
                 left_right_velocity = 0
 
-        # Salir
+        # Press ESC to quit
         if k == 27:
-            Main_Real = True
+            dron_continuos_cycle = False
             break
 
+        # Take 4 points from the frame 
         pts1 = np.float32([[140, 0], [820, 0], [0, 666], [960, 660]])
+        # Make the new screen size 
         pts2 = np.float32([[0, 0], [960, 0], [0, 720], [960, 720]])
+        
+        # Change the perspective of the frame to counteract the camera angle
         matrix = cv2.getPerspectiveTransform(pts1, pts2)
         frame = cv2.warpPerspective(frame, matrix, (960, 720))
 
-        # Getting the position of the object, radius and tracking the object in the frame
+        # If we are in debug mode, send the track bars values to the object detection algorithm
         if args.debug:
-            x, y, r, detection, frame_processed = processing(frame, lower_hsv, upper_hsv)
+            x, y, r, detection, frame_processed = object_detection(frame, lower_hsv, upper_hsv)
         else:
-            x, y, r, detection, frame_processed = processing(frame, 0, 0)
+            x, y, r, detection, frame_processed = object_detection(frame, 0, 0)
 
-        # Display grid in the actual frame, take video and radius of the object as arguments
-        # return the grid dynamic position first line passing through  x_1 ..... last line trough y_2
-        x_1, x_2, y_1, y_2, frame_grid = display_grid(frame_processed, 100, x, y)
+        # Display grid in the actual frame
+        x_1, x_2, y_1, y_2, frame_grid = display_grid(frame_processed, grid_size, x, y)
 
-        # display battery and logo in the video
+        # Display battery and logo in the video
         frame_user = display_battery(display_text(frame_grid))
 
+        # Autonomous mode
         if send_rc_control and not OVERRIDE:
 
             if not args.debug:
+                # Eliminate pass values
                 left_right_velocity = 0
                 for_back_velocity = 0
                 up_down_velocity = 0
@@ -471,23 +476,23 @@ while True:
 
                 if detection:
                     yaw_velocity, up_down_velocity, for_back_velocity = drone_stay_close(x, y, x_1, x_2, y_1,
-                                                                                                   y_2, r, 40, 5)
-            # Display information to the user
-            print(f"counter = {counter}")
-        # Update counter
-        counter = counter + 1
-        # Display the video
-        cv2.imshow('Drone X', frame_user)
-
+                                                                                y_2, r, radius_stop, radius_stop_tolerance)
+        # Save the video session if True
         if args.save_session:
             writer.write(frame_original)
-            writer_proccesed.write(frame_user)
+            writer_processed.write(frame_user)
+
+        # Display the video
+        cv2.imshow('Drone X', frame_user)
+        # Delay to showcase desired fps in video
+        time.sleep(1 / FPS)
+        # Update takeoff timer
+        takeoff_timer += 1
 
     break
 
 cv2.destroyAllWindows()
 
-print("Adios Vaquero")
+print("Goodbye")
 tello.get_battery()
-
 tello.end()
