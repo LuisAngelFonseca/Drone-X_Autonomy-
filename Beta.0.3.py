8b94a6bfd137bdd06e9ff1de67dce774cc72be77
Kamil <a01702974@itesm.mx>
2020-05-26 19:29:55 -0500

    Changed variable is_flying_send_rc_control to library tello.is_flying

--- Beta.0.3.py
+++ Beta.0.3.py
@@ -276,8 +276,6 @@ def drone_stay_close(x, y, limit_x1, limit_x2, limit_y1, limit_y2, radius, dista
 # Setup
 # Create an instance of a Drone from the Tello library
 tello = Tello()
-# Variable to check if drone is flying
-is_flying_send_rc_control = False
 # Create a taking off boolean
 is_taking_off = False
 # Create a landing boolean
@@ -297,7 +295,7 @@ battery = 0
 # Grid size
 grid_size = 100
 # Radius of the object in which the drone will stop
-radius_stop = 90
+radius_stop = 60
 # Tolerance range in which the drone will stop
 radius_stop_tolerance = 5
 
@@ -399,7 +397,7 @@ while True:
 
         # --------------------------- SEND DRONE VELOCITY SECTION -----------------------------
         # Function that updates drone velocities in the override mode and autonomous mode
-        if is_flying_send_rc_control:
+        if tello.is_flying:
             tello.send_rc_control(left_right_velocity, for_back_velocity, up_down_velocity, yaw_velocity)
 
         # --------------------------- FRAME READ SECTION -----------------------------
@@ -475,7 +473,7 @@ while True:
 
         # Press ESC to quit -!-!-!-> EXIT PROGRAM <-!-!-!-
         if k == 27:
-            if is_flying_send_rc_control:
+            if tello.is_flying:
                 for i in range(50):
                     tello.send_rc_control(0, 0, 0, 0)  # Stop the drone if it has momentum
                     time.sleep(1 / FPS)
@@ -485,11 +483,10 @@ while True:
         if is_taking_off:
             tello.get_battery()
             tello.takeoff()
-            is_flying_send_rc_control = True
             is_taking_off = False
 
         # Press T to take off
-        if (k == ord('t') or k == ord('T')) and not is_flying_send_rc_control and not args.debug:
+        if (k == ord('t') or k == ord('T')) and not tello.is_flying and not args.debug:
             is_taking_off = True
             print('Takeoff...')
             frame_user = display_text(frame_user, 'Taking off...', (5, 25), (0, 255, 255))
@@ -499,17 +496,16 @@ while True:
                 tello.send_rc_control(0, 0, 0, 0)  # Stop the drone if it has momentum
                 time.sleep(1 / FPS)
             tello.land()
-            is_flying_send_rc_control = False
             is_landing = False
 
         # Press L to land
-        if (k == ord('l') or k == ord('L')) and is_flying_send_rc_control and not args.debug:
+        if (k == ord('l') or k == ord('L')) and tello.is_flying and not args.debug:
             is_landing = True
             print('Land...')
             frame_user = display_text(frame_user, 'Landing...', (5, 25), (0, 255, 255))
 
         # Press spacebar to enter override mode
-        if k == 32 and is_flying_send_rc_control:
+        if k == 32 and tello.is_flying:
             if not OVERRIDE:
                 OVERRIDE = True
                 print('OVERRIDE ENABLED')
@@ -551,7 +547,7 @@ while True:
                 left_right_velocity = 0
 
         # --------------------------- AUTONOMOUS SECTION -----------------------------
-        if is_flying_send_rc_control and not OVERRIDE and not args.debug:
+        if tello.is_flying and not OVERRIDE and not args.debug:
             # Eliminate pass values
             left_right_velocity = 0
             for_back_velocity = 0
